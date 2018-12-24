8e7c587b49abf211ae513784205235fae2cab9fe
Leonardo Kramer <kramer.leo.94@gmail.com>
2018-12-24 20:10:08 -0300

    Login utilizando Facebook, Twitter e Google pronto. Falta beautify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+
+import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service'
+import { UserService } from './services/user.service'
+import { UserResolver } from './components/main-menu/user.resolver'
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/main-menu/user.resolver.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, Router } from "@angular/router";
+import { UserService } from '../../services/user.service';
+import { FirebaseUserModel } from '../../models/user.model';
+
+@Injectable()
+export class UserResolver implements Resolve<FirebaseUserModel> {
+
+  constructor(public userService: UserService, private router: Router) { }
+
+  resolve(route: ActivatedRouteSnapshot) : Promise<FirebaseUserModel> {
+
+    let user = new FirebaseUserModel();
+
+    return new Promise((resolve, reject) => {
+      this.userService.getCurrentUser()
+      .then(res => {
+        user.image = res.photoURL;
+        user.name = res.displayName;
+        user.provider = res.providerData[0].providerId;
+        
+        if(user.provider == "facebook.com") {
+          user.image += "?height=500";
+        } else if(user.provider == "twitter.com") {
+          var image : String = user.image;
+          var splitted = image.split("/");
+          var param1 = splitted[4];
+          
+          splitted = splitted[5].split("_");
+          var param2 = splitted[0];
+
+          user.image = "https://pbs.twimg.com/profile_images/" + param1 + "/" + param2 + ".jpg";
+        }
+        return resolve(user);
+      }, err => {
+        this.router.navigate(['/login']);
+        return reject(err);
+      })
+    })
+  }
+}
